0278e33d858566cb4fc0149152f6b9d82472c9f2
brmcdoug <brmcdoug@cisco.com>
2023-05-13 15:38:29 -0700

    hosts

--- util/scapy/srv6-probe-vrf2.py
+++ util/scapy/srv6-probe-vrf2.py
@@ -13,5 +13,5 @@ p0 = Ether(src = "fe:54:00:67:43:8b", dst = "52:54:00:74:c1:02") \
 / IPv6(src = "2001:0:102:2::3", dst = "fc00:0:12:1:6600::") \
 / IP(src = "10.102.12.1", dst = "10.102.2.3") / ICMP()
 p0.show()
-sendp(p0, iface="sonic01-16", count=64)
+sendp(p0, iface="sonic02-20", count=64)
 
