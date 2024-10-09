06d9a0222c18af7e20bc7fe4421e650e8ea8a8fc
brmcdoug <brmcdoug@cisco.com>
2024-10-08 21:30:27 -0700

    bridges

--- util/add-bridges.sh
+++ util/add-bridges.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
  brctl addbr sonic01-16
+brctl addbr sonic01-20
  brctl addbr sonic02-16
  brctl addbr sonic02-20
  brctl addbr k8s-cp-node00
@@ -9,6 +10,7 @@
  brctl addbr k8s-wkr-node02
 
  ip link set up sonic01-16
+ ip link set up sonic01-20
  ip link set up sonic02-16
  ip link set up sonic02-20
  ip link set up k8s-cp-node00
--- util/del-bridges.sh
+++ util/del-bridges.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
  ip link set down sonic01-16
+ ip link set down sonic01-20
  ip link set down sonic02-16
  ip link set down sonic02-20
  ip link set down k8s-cp-node00
@@ -8,6 +9,7 @@
  ip link set down k8s-wkr-node02
 
  brctl delbr sonic01-16
+ brctl delbr sonic01-20
  brctl delbr sonic02-16
  brctl delbr sonic02-20
  brctl delbr k8s-cp-node00
