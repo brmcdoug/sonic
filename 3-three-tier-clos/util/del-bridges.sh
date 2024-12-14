62638ccd6ff2c4d54ece21d7d01e2c741043d64b
brmcdoug <brmcdoug@cisco.com>
2024-12-14 11:00:13 -0800

    br

--- 3-three-tier-clos/util/del-bridges.sh
+++ 3-three-tier-clos/util/del-bridges.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
- ip link set down cp-node00-0
- ip link set down cp-node00-1
- ip link set down cp-node00-2
- ip link set down cp-node00-3
+#  ip link set down cp-node00-0
+#  ip link set down cp-node00-1
+#  ip link set down cp-node00-2
+#  ip link set down cp-node00-3
 
- ip link set down wkr-node01-0
- ip link set down wkr-node01-1
- ip link set down wkr-node01-2
- ip link set down wkr-node01-3
+#  ip link set down wkr-node01-0
+#  ip link set down wkr-node01-1
+#  ip link set down wkr-node01-2
+#  ip link set down wkr-node01-3
 
- ip link set down wkr-node02-0
- ip link set down wkr-node02-1
- ip link set down wkr-node02-2
- ip link set down wkr-node02-3
+#  ip link set down wkr-node02-0
+#  ip link set down wkr-node02-1
+#  ip link set down wkr-node02-2
+#  ip link set down wkr-node02-3
 
- ip link set down wkr-node03-0
- ip link set down wkr-node03-1
- ip link set down wkr-node03-2
- ip link set down wkr-node03-3
+#  ip link set down wkr-node03-0
+#  ip link set down wkr-node03-1
+#  ip link set down wkr-node03-2
+#  ip link set down wkr-node03-3
 
- brctl delbr cp-node00-0
- brctl delbr cp-node00-1
- brctl delbr cp-node00-2
- brctl delbr cp-node00-3
- brctl delbr wkr-node01-0
- brctl delbr wkr-node01-1
- brctl delbr wkr-node01-2
- brctl delbr wkr-node01-3
- brctl delbr wkr-node02-0
- brctl delbr wkr-node02-1
- brctl delbr wkr-node02-2
- brctl delbr wkr-node02-3
- brctl delbr wkr-node03-0
- brctl delbr wkr-node03-1
- brctl delbr wkr-node03-2
- brctl delbr wkr-node03-3
+#  brctl delbr cp-node00-0
+#  brctl delbr cp-node00-1
+#  brctl delbr cp-node00-2
+#  brctl delbr cp-node00-3
+#  brctl delbr wkr-node01-0
+#  brctl delbr wkr-node01-1
+#  brctl delbr wkr-node01-2
+#  brctl delbr wkr-node01-3
+#  brctl delbr wkr-node02-0
+#  brctl delbr wkr-node02-1
+#  brctl delbr wkr-node02-2
+#  brctl delbr wkr-node02-3
+#  brctl delbr wkr-node03-0
+#  brctl delbr wkr-node03-1
+#  brctl delbr wkr-node03-2
+#  brctl delbr wkr-node03-3
 
 
 
